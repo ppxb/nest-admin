7eefe39b6df0f63f0e3d783917cc9c301a5f1346
ppxb <317842449@qq.com>
2026-05-21 14:45:44 +0800

    refactor: move logger module options creation to bootstrap

---
+++ src/bootstrap/logger.ts
@@ -0,0 +1,26 @@
+import { RequestMethod } from '@nestjs/common'
+import type { Params } from 'nestjs-pino'
+
+export function createLoggerOptions(logLevel: string, isDev: boolean): Params {
+  return {
+    forRoutes: [{ method: RequestMethod.ALL, path: '*wildcard' }],
+    pinoHttp: {
+      level: logLevel,
+      transport: isDev
+        ? {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              singleLine: true,
+              messageFormat: '[{context}] {msg}',
+              ignore: 'hostname,context,req,res,pid,responseTime'
+            }
+          }
+        : undefined,
+      customProps: () => ({ context: 'HTTP' }),
+      customSuccessMessage: (req, res, responseTime) =>
+        `✨ ${req.method} ${req.url} - ${res.statusCode} (${responseTime}ms)`,
+      customErrorMessage: (req, _, err) => `❌ ${req.method} ${req.url} failed | ${err.message}`
+    }
+  }
+}
